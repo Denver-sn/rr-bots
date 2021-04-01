e09fcd618f442e463644fa5844b2148893f1c9f2
Denver <50638006+Denver-sn@users.noreply.github.com>
2021-04-01 14:42:58 +0000

    Update rr-bot_Auto-refil.py

--- rr-bot_Auto-refil.py
+++ rr-bot_Auto-refil.py
@@ -6,11 +6,13 @@ from pyvirtualdisplay import Display
 import os
 
 #Setup of the driver (Chromedriver)
-chrome_options = Options()
-chrome_options.binary_location = GOOGLE_CHROME_BIN
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--no-sandbox')
-driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
 
 
 #Set the accounts here
